158cf7f004700c4055b5fd0845013f01b41b99b4
Tulio Alves <tulioalves17@gmail.com>
2025-04-07 15:30:30 -0300

    fix
    fix

--- database/migrations/2025_04_05_184410_song.php
+++ database/migrations/2025_04_05_184410_song.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Song;
 
 return new class extends Migration
 {
@@ -19,6 +20,56 @@ return new class extends Migration
             $table->string('image')->nullable();
             $table->timestamps();
         });
+
+        // Criar lista de musicas padrao
+        Song::create([
+            'title' => 'Tião Carreiro e Pardinho - Rei do Gado',
+            'viewCount' => 5948124,
+            'link' => 'https://www.youtube.com/watch?v=bv3593lmltY',
+            'image' => 'https://i.ytimg.com/vi/bv3593lmltY/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-BIAC4AOKAgwIABABGGUgTChBMA8=&rs=AOn4CLArVX5qg_FsQlEMh61E9wrVv_8FIw'
+        ]);
+
+        Song::create([
+            'title' => 'Tião Carreiro e Pardinho - Rei do Gado',
+            'viewCount' => 5948124,
+            'link' => 'https://www.youtube.com/watch?v=bv3593lmltY',
+            'image' => 'https://i.ytimg.com/vi/bv3593lmltY/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-BIAC4AOKAgwIABABGGUgTChBMA8=&rs=AOn4CLArVX5qg_FsQlEMh61E9wrVv_8FIw'
+        ]);
+
+        Song::create([
+            'title' => 'Tião Carreiro e Pardinho - Rei do Gado',
+            'viewCount' => 5948124,
+            'link' => 'https://www.youtube.com/watch?v=bv3593lmltY',
+            'image' => 'https://i.ytimg.com/vi/bv3593lmltY/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-BIAC4AOKAgwIABABGGUgTChBMA8=&rs=AOn4CLArVX5qg_FsQlEMh61E9wrVv_8FIw'
+        ]);
+
+        Song::create([
+            'title' => 'Tião Carreiro e Pardinho - Rei do Gado',
+            'viewCount' => 5948124,
+            'link' => 'https://www.youtube.com/watch?v=bv3593lmltY',
+            'image' => 'https://i.ytimg.com/vi/bv3593lmltY/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-BIAC4AOKAgwIABABGGUgTChBMA8=&rs=AOn4CLArVX5qg_FsQlEMh61E9wrVv_8FIw'
+        ]);
+
+        Song::create([
+            'title' => 'Tião Carreiro e Pardinho - Rei do Gado',
+            'viewCount' => 5948124,
+            'link' => 'https://www.youtube.com/watch?v=bv3593lmltY',
+            'image' => 'https://i.ytimg.com/vi/bv3593lmltY/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-BIAC4AOKAgwIABABGGUgTChBMA8=&rs=AOn4CLArVX5qg_FsQlEMh61E9wrVv_8FIw'
+        ]);
+
+        Song::create([
+            'title' => 'Tião Carreiro e Pardinho - Rei do Gado',
+            'viewCount' => 5948124,
+            'link' => 'https://www.youtube.com/watch?v=bv3593lmltY',
+            'image' => 'https://i.ytimg.com/vi/bv3593lmltY/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-BIAC4AOKAgwIABABGGUgTChBMA8=&rs=AOn4CLArVX5qg_FsQlEMh61E9wrVv_8FIw'
+        ]);
+
+        Song::create([
+            'title' => 'Tião Carreiro e Pardinho - Rei do Gado',
+            'viewCount' => 5948124,
+            'link' => 'https://www.youtube.com/watch?v=bv3593lmltY',
+            'image' => 'https://i.ytimg.com/vi/bv3593lmltY/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-BIAC4AOKAgwIABABGGUgTChBMA8=&rs=AOn4CLArVX5qg_FsQlEMh61E9wrVv_8FIw'
+        ]);
     }
 
     /**
